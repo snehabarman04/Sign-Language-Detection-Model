cf8992b6c216933aab4d465b84fb0da0bfff3472
Sneha Barman <134500649+snehabarman04@users.noreply.github.com>
2024-07-08 00:25:53 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,14 +4,12 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping
 
-# Define constants
 train_dir = r'C:\Users\SNEHA\PycharmProjects\handsigndetection\CNN\processed_Train_Alphabet'
 test_dir = r'C:\Users\SNEHA\PycharmProjects\handsigndetection\CNN\processed_Test_Alphabet'
-img_width, img_height = 513, 512  # Assuming your processed images are 512x512
+img_width, img_height = 513, 512  
 batch_size = 32
-num_classes = 27  # Number of classes (A-Z + Blank)
+num_classes = 27  
 
-# ImageDataGenerator for training and validation data
 train_datagen = ImageDataGenerator(rescale=1. / 255)
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
@@ -27,7 +25,6 @@ validation_generator = test_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='categorical')
 
-# Define the CNN model
 model = Sequential([
     Conv2D(32, (3, 3), activation='relu', input_shape=(img_width, img_height, 3)),
     MaxPooling2D((2, 2)),
@@ -41,15 +38,12 @@ model = Sequential([
     Dense(num_classes, activation='softmax')
 ])
 
-# Compile the model
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-# Early stopping callback to prevent overfitting
 early_stopping = EarlyStopping(monitor='val_loss', patience=3)
 
-# Train the model
 history = model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples // batch_size,
@@ -58,7 +52,5 @@ history = model.fit(
     validation_steps=validation_generator.samples // batch_size,
     callbacks=[early_stopping])
 
-# Save the trained model
 model.save('sign_language_model.h5')
-
 print("Model training completed and saved as 'sign_language_model.h5'")
